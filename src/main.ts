d2ab456f68419b3ea2538d5b93c7f82e27008bce
tsiotska <vitaliytsisaruk@gmail.com>
2025-09-28 03:48:28 +0300

    Prisma module imported properly.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import * as cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const DEV_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
@@ -13,13 +14,23 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: (origin, cb) =>
+    origin: (origin: string, cb: (arg0: null, arg1: any) => any) =>
       cb(
         null,
         origin && DEV_ORIGINS.includes(origin) ? origin : DEV_ORIGINS[0],
       ),
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Algotcha API')
+    .setDescription('API docs for Auth, Comments, Exchange')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT || 8080);
 }
