5f8bed8512bee62e734bb3016ef11e45c7fb6763
Andrii Liudvichuk <amlyudvichuk@gmail.com>
2025-12-05 01:21:42 +0200

    fix: add PrismaService to ExchangeModule

--- src/modules/exchange/exchange.module.ts
+++ src/modules/exchange/exchange.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 import { ExchangeController } from './exchange.controller';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
-  providers: [ExchangeService],
+  providers: [ExchangeService, PrismaService],
   controllers: [ExchangeController],
   exports: [ExchangeService],
 })
